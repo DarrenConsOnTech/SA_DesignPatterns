1532fe03d6ceffd727a691b02a31ba4572568891
Ram Baldeo <77577818+rambaldeo@users.noreply.github.com>
2022-10-05 13:13:25 -0400

    Add files via upload

---
+++ Assignment1Final/BananaFactoryFinal.java
@@ -0,0 +1,34 @@
+package Assignment1Final;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BananaFactoryFinal implements GroceryProductFactoryFinal{
+
+	@Override
+	public void CreateProduct(int id) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		String toFind=String.valueOf(id);
+		getPrice(toFind);
+		System.out.println("ID	Name	Price");
+		System.out.println(getPrice(toFind));
+		
+	}
+
+	private String getPrice(String toFind) throws FileNotFoundException{
+		// TODO Auto-generated method stub
+		Scanner search = new Scanner(new File("/Volumes/University/NewEclipse/Architecture/src/Assignment1Final/BananaProducts.txt"));
+		while (search.hasNext()) {
+			String BananaLine = search.nextLine();
+			if (BananaLine.contains(toFind)) {
+				
+				return BananaLine;
+			}
+			
+		}
+		System.out.println("There is no banana product with that ID");
+		return null;
+	}
+
+}
